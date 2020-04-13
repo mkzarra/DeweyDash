8ae811d27aa81cad17b514dd7f7f0194b5f2fd3f
Mike Zarra <mkzarra@Mikes-Air.fios-router.home>
2020-04-13 10:24:48 -0400

    minimize spaces

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,7 @@
 class UserController < ApplicationController
   def show
     key = "HvEp40NMG2XEUvVtxvrIg"
-    secret =  "9btoslPmCwBxLeCngBMMSjlg2HJfJNsYqhEyREpnI"
+    secret = "9btoslPmCwBxLeCngBMMSjlg2HJfJNsYqhEyREpnI"
     consumer = OAuth::Consumer.new(key, secret, { :site => "https://www.goodreads.com" })
     access_token = OAuth::AccessToken.new(consumer, session[:access_token], session[:access_secret])
     response = access_token.get("https://www.goodreads.com/api/auth_user")
