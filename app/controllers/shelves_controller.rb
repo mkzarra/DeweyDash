f8c61d484df5d480beb929e130242f849fb26c0e
Mike Zarra <mkzarra@Mikes-Air.fios-router.home>
2020-05-03 12:48:32 -0400

    get shelf in show

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -3,12 +3,12 @@ class ShelvesController < ApplicationController
     response = goodreads_oauth_access_token.get("https://www.goodreads.com/shelf/list.xml?per_page=200")
     response_hash = Hash.from_xml(response.body)
     @shelves = response_hash["GoodreadsResponse"]['shelves']['user_shelf']
-    binding.pry
   end
 
   def show
-    response = goodreads_oauth_access_token.get("https://www.goodreads.com/shelf/list.xml?id=#{params[:id]}")
+    response = goodreads_oauth_access_token.get("https://www.goodreads.com/shelf/list/#{params[:id]}.xml?v=2")
     response_hash = Hash.from_xml(response.body)
-    @shelf = response_hash["GoodreadsResponse"]["shelves"]['user_shelf'][0]
+    @shelf = response_hash["GoodreadsResponse"]['shelves']['user_shelf'].find { |shelf| shelf['id'].to_s == params[:id] }
+    # @reading_list = @books.filter { |book| book['my-review']['shelves']['shelf']['name'] == @shelf['name'] }
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,9 @@
 class UserController < ApplicationController
   def show
+    key = "HvEp40NMG2XEUvVtxvrIg"
+    secret = "9btoslPmCwBxLeCngBMMSjlg2HJfJNsYqhEyREpnI"
+    consumer = OAuth::Consumer.new(key, secret, { :site => "https://www.goodreads.com" })
+    access_token = OAuth::AccessToken.new(consumer, session[:access_token], session[:access_secret])
     response = access_token.get("https://www.goodreads.com/api/auth_user")
     response_hash = Hash.from_xml(response.body)
     @user = response_hash['GoodreadsResponse']['user']
